e191efc1ccd93dc40b7214d7a4fa33ff8462e021
Benjamin Lee <825803+bltb@users.noreply.github.com>
2025-09-13 03:51:58 +1000

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'bacon lettuce tomato leche'
+copyright = '2025, Benjamin Lee'
+author = 'Benjamin Lee'
+release = '0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+todo_include_todos = True
