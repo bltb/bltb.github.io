9d87623ea4a40d375f326b4b1bcb84a830a0dd3c
Benjamin Lee <825803+bltb@users.noreply.github.com>
2025-09-14 09:12:18 +1000

    Do f strings work?

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,33 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 
-project = 'bacon lettuce tomato leche'
-copyright = '2025, Benjamin Lee'
-author = 'Benjamin Lee'
-release = '0.1'
+sys.path.insert(0, os.path.abspath("."))
+
+project = "bacon lettuce tomato leche"
+copyright = "2025, Benjamin Lee"
+author = "Benjamin Lee"
+release = "0.1"
 
 extensions = [
     "canonical_sphinx",
     "sphinx_last_updated_by_git",
     "sphinx.ext.intersphinx",
     "sphinx_sitemap",
-    'sphinxcontrib.mermaid',
+    "sphinxcontrib.mermaid",
 ]
 
+github_username = "bltb"
+github_repository = "bltb.github.io"
+github_url = f"https://github.com/{github_username}{github_repository}"
+
 html_context = {
-    "github_url": "https://github.com/bltb/bltb.github.io",
+    "github_url": github_url,
     "github_version": "master",
     "github_folder": "/docs/",
+}
+
+html_theme_options = {
+    "source_edit_link": github_url,
 }
 
 disable_feedback_button = True
